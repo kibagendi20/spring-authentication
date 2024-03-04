088f62c1520fb6c361f328fd3c01707a85855502
kibagendi20 <wesoon98@gmail.com>
2024-03-04 13:20:06 +0300

    Defined custom UserDetailService class

--- src/main/java/com/security/authentication/entity/MyUserDetailService.java
+++ src/main/java/com/security/authentication/entity/MyUserDetailService.java
@@ -1,4 +1,29 @@
 package com.security.authentication.entity;
 
-public class MyUserDetailService {
+import com.security.authentication.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userRes = userRepo.findByEmail(email);
+        if (userRes.isEmpty()) throw new  UsernameNotFoundException("Could not find user with email = " + email);
+        User user = userRes.get();
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
 }
