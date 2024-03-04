dc806934433000d90485d2366bbc6492ea10bb8d
kibagendi20 <wesoon98@gmail.com>
2024-03-04 13:22:21 +0300

    User repository definition

--- src/main/java/com/security/authentication/repository/UserRepo.java
+++ src/main/java/com/security/authentication/repository/UserRepo.java
@@ -1,4 +1,10 @@
 package com.security.authentication.repository;
 
-public interface UserRepo {
+import com.security.authentication.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends MongoRepository<User, String> {
+    public Optional<User> findByEmail(String email);
 }
